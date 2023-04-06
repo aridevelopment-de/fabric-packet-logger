b4e1fbd12402e96e2fc7554e56bfcaa87aea8aaf
Ari van Houten <ari.publicmail@gmail.com>
2023-04-06 22:37:57 +0200

    Added new base packet handler structure to support wiki.vg url and descriptions

--- src/main/java/de/ari24/packetlogger/packets/BasePacketHandler.java
+++ src/main/java/de/ari24/packetlogger/packets/BasePacketHandler.java
@@ -1,10 +1,17 @@
 package de.ari24.packetlogger.packets;
 
+import com.google.gson.JsonNull;
 import com.google.gson.JsonObject;
-import net.minecraft.client.world.ClientWorld;
 import net.minecraft.network.packet.Packet;
 
 public interface BasePacketHandler<T extends Packet<?>> {
-    String id();
+    String name();
+    String url();
+    default JsonObject description() {
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("general", "No description available.");
+        jsonObject.add("wikiVgNotes", JsonNull.INSTANCE);
+        return jsonObject;
+    }
     JsonObject serialize(T packet);
 }
