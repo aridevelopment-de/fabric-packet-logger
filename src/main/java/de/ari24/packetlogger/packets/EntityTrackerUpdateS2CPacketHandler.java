55de582fccfe1f9cd60f63cfb46630c156286684
Marco <mneuhaus44@gmail.com>
2023-04-17 12:17:47 +0200

    Fix EntityTracker values always be empty

--- src/main/java/de/ari24/packetlogger/packets/EntityTrackerUpdateS2CPacketHandler.java
+++ src/main/java/de/ari24/packetlogger/packets/EntityTrackerUpdateS2CPacketHandler.java
@@ -1,12 +1,11 @@
 package de.ari24.packetlogger.packets;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonNull;
 import com.google.gson.JsonObject;
 import de.ari24.packetlogger.utils.ConvertUtils;
 import net.minecraft.entity.data.DataTracker;
 import net.minecraft.network.packet.s2c.play.EntityTrackerUpdateS2CPacket;
-
-import java.util.ArrayList;
 
 public class EntityTrackerUpdateS2CPacketHandler implements BasePacketHandler<EntityTrackerUpdateS2CPacket> {
     @Override
@@ -34,12 +33,12 @@ public class EntityTrackerUpdateS2CPacketHandler implements BasePacketHandler<En
         JsonObject jsonObject = new JsonObject();
         ConvertUtils.appendEntity(jsonObject, packet.id());
 
-        ArrayList<JsonObject> list = new ArrayList<>();
-
+        JsonArray list = new JsonArray(packet.trackedValues().size());
         for (DataTracker.SerializedEntry<?> entry : packet.trackedValues()) {
             JsonObject jsonObject1 = new JsonObject();
             jsonObject1.addProperty("id", entry.id());
             jsonObject1.addProperty("value", entry.value().toString());
+            list.add(jsonObject1);
         }
 
         jsonObject.add("trackedValues", ConvertUtils.GSON_INSTANCE.toJsonTree(list));
