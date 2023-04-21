cd916a2ed60da82f195318bd2814f8590ae83f7c
Ari van Houten <ari.publicmail@gmail.com>
2023-04-21 15:23:30 +0200

    Added toast notification to logstate switch (when websocket sends)

--- src/main/java/de/ari24/packetlogger/web/handlers/PacketloggerLogstateHandler.java
+++ src/main/java/de/ari24/packetlogger/web/handlers/PacketloggerLogstateHandler.java
@@ -3,6 +3,7 @@ package de.ari24.packetlogger.web.handlers;
 import com.google.gson.JsonObject;
 import de.ari24.packetlogger.PacketLogger;
 import de.ari24.packetlogger.config.PacketLoggerConfigModel;
+import de.ari24.packetlogger.ui.PacketLoggerToast;
 import org.java_websocket.WebSocket;
 
 public class PacketloggerLogstateHandler implements BaseHandler {
@@ -10,6 +11,8 @@ public class PacketloggerLogstateHandler implements BaseHandler {
     public void handle(WebSocket ws, JsonObject data) {
         // TODO: Add exception handling
         int state = data.get("data").getAsInt();
-        PacketLogger.CONFIG.logState(PacketLoggerConfigModel.LogState.values()[state]);
+        PacketLoggerConfigModel.LogState newState = PacketLoggerConfigModel.LogState.values()[state];
+        PacketLogger.CONFIG.logState(newState);
+        PacketLoggerToast.notify("Logging state: " + newState.name().toLowerCase());
     }
 }
